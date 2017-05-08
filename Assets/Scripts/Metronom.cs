bf208cb39750516f9f71de0372a6b2046586275c
Thomas Pommerening <pommermann@hotmail.de>
2017-05-08 17:12:03 +0200

    Added Metronom Visuals + some minor script improvments. Visual Metronom Asynchron. Lines of the Visualisation all in corret position DONT CHANGE!

--- Assets/Scripts/Metronom.cs
+++ Assets/Scripts/Metronom.cs
@@ -9,11 +9,21 @@ public class Metronom : MonoBehaviour {
     public static int step;
     public int maxStep;
     public bool run = true;
+
+    GameObject line;
+    Transform[] checkPoints =  new Transform[9];
     
     bool nextStep = true;
     // Use this for initialization
     void Start () {
-        
+        line = GameObject.Find("Line");
+        checkPoints[0] = GameObject.Find("StartPoint").transform;
+        for (int i = 1; i < 8; i++)
+        {
+            checkPoints[i] = GameObject.Find("Point"+i).transform;
+        }
+        checkPoints[8] = GameObject.Find("Endpoint").transform;
+
 	}
 	
 	// Update is called once per frame
@@ -37,5 +47,9 @@ public class Metronom : MonoBehaviour {
         }
         nextStep = true;
         Debug.Log(step);
+    }
+    void Visualize()
+    {
+
     }
 }
---
+++ Assets/Scripts/MetronomVisualLine.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MetronomVisualLine : MonoBehaviour {
+
+    RectTransform rectTransform;
+    Vector3 startPosition;
+    Vector3 target;
+    float t;
+    float timeToReachTarget = 8;
+
+	// Use this for initialization
+	void Start () {
+        rectTransform = GetComponent<RectTransform>();
+        startPosition = rectTransform.position;
+        target = GameObject.Find("Endpoint").transform.position;
+	}
+	
+	// Update is called once per frame
+
+        //Asynchron  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! nach einem durchgang
+	void Update () {
+        t += Time.deltaTime / timeToReachTarget;
+        transform.position = Vector3.Lerp(startPosition, target, t);
+        if(transform.position == target)
+        {
+            transform.position = startPosition;  // hier noch fixen
+            Debug.Log("ok");
+            t = 0;
+        }
+
+    }
+}
